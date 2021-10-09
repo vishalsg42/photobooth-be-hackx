121d12e23b3e6fe0c638079676614cb769a4993a
vishalsg42 <vishalsg42@gmail.com>
2021-10-09 14:25:06 +0530

    added middleware & mongo connection

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import httpContext from "express-http-context";
 import "reflect-metadata";
 import fileUpload from 'express-fileupload';
 
+
 // Application imports
 import router from "./routes/urls";
+import logger from "./services/winstonLogging";
+import { errorHandlingMiddleware, responseMiddleware, requestMiddleware } from "./middleware";
+import mongoConnect from "./db/mongoConnection";
 
 const app = express();
 const PORT = process.env.PORT || 8008;
@@ -29,8 +33,24 @@ app.use(httpContext.middleware);
 app.use(express.static("public"));
 app.use(express.static("storage/tmp"));
 
+// Middleware
+app.use(requestMiddleware);
+
+app.use(responseMiddleware);
+
 // Routes will always go here
 app.use("/", router);
+
+app.use(errorHandlingMiddleware);
+
+//staring mongo connection
+mongoConnect(() => {
+    console.log("Mongo server connected");
+});
+
+// setting up loggers
+const globalAny: any = global;
+globalAny.logger = logger();
 
 // exiting all the process before terminating the app
 process.on("SIGINT", () => {
